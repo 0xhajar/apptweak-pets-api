5b5d103985e3ea50bdd2b0fc2c50c7ec4bc02699
0xhajar <hajar.haouat@student.vinci.be>
2024-05-16 23:14:25 +0200

    refactor: using .env for the db

--- models/db_conf.js
+++ models/db_conf.js
@@ -1,6 +1,6 @@
-const config = require('../config.js');
+require('dotenv').config();
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(config.dbPath);
+const db = new sqlite3.Database(process.env.DB_PATH);
 
 module.exports = db;
